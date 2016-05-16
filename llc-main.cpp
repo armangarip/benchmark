bc759858053b2bfd474db58708b0c998eed5910f
aktemur <baris.aktemur@ozyegin.edu.tr>
2016-05-16 17:48:26 +0300

    llc-benchmark adapted to LLVM 3.5.0.

--- llc-main.cpp
+++ llc-main.cpp
@@ -3,36 +3,18 @@
 #include <ctime>
 #include <fstream>
 #include <sstream>
-#include "llvm/ExecutionEngine/GenericValue.h"
-#include "llvm/ExecutionEngine/Interpreter.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/ExecutionEngine/MCJIT.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IRReader/IRReader.h"
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/Support/ManagedStatic.h"
-#include "llvm/Support/TargetSelect.h"
-
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/ADT/Triple.h"
 #include "llvm/CodeGen/CommandFlags.h"
 #include "llvm/CodeGen/LinkAllAsmWriterComponents.h"
 #include "llvm/CodeGen/LinkAllCodegenComponents.h"
-#include "llvm/CodeGen/MIRParser/MIRParser.h"
 #include "llvm/IR/DataLayout.h"
 #include "llvm/IR/IRPrintingPasses.h"
 #include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/Verifier.h"
 #include "llvm/IRReader/IRReader.h"
 #include "llvm/MC/SubtargetFeature.h"
 #include "llvm/Pass.h"
+#include "llvm/PassManager.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/FileSystem.h"
@@ -46,35 +28,34 @@
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/ToolOutputFile.h"
+#include "llvm/Target/TargetLibraryInfo.h"
 #include "llvm/Target/TargetMachine.h"
-#include "llvm/Target/TargetSubtargetInfo.h"
 #include <memory>
 
 #include "llvm/ADT/StringMap.h"
 #include "llvm/DebugInfo/DIContext.h"
-#include "llvm/DebugInfo/DWARF/DWARFContext.h"
-#include "llvm/ExecutionEngine/RTDyldMemoryManager.h"
+#include "llvm/ExecutionEngine/ObjectBuffer.h"
+#include "llvm/ExecutionEngine/ObjectImage.h"
 #include "llvm/ExecutionEngine/RuntimeDyld.h"
 #include "llvm/ExecutionEngine/RuntimeDyldChecker.h"
 #include "llvm/MC/MCAsmInfo.h"
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCDisassembler.h"
-#include "llvm/MC/MCInstPrinter.h"
 #include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCInstPrinter.h"
 #include "llvm/MC/MCRegisterInfo.h"
-#include "llvm/MC/MCSubtargetInfo.h"
 #include "llvm/Object/MachO.h"
-#include "llvm/Object/SymbolSize.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/DynamicLibrary.h"
 #include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/Memory.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/Signals.h"
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/TargetSelect.h"
-#include "llvm/Support/raw_ostream.h"
+#include <system_error>
 
 
 using namespace llvm;
@@ -121,26 +102,6 @@ public:
     // explicit cache flush, otherwise JIT code manipulations (like resolved
     // relocations) will get to the data cache but not to the instruction cache.
     virtual void invalidateInstructionCache();
-    
-    void addDummySymbol(const std::string &Name, uint64_t Addr) {
-        DummyExterns[Name] = Addr;
-    }
-    
-    RuntimeDyld::SymbolInfo findSymbol(const std::string &Name) override {
-        auto I = DummyExterns.find(Name);
-        
-        if (I != DummyExterns.end())
-            return RuntimeDyld::SymbolInfo(I->second, JITSymbolFlags::Exported);
-            
-            return RTDyldMemoryManager::findSymbol(Name);
-            }
-    
-    void registerEHFrames(uint8_t *Addr, uint64_t LoadAddr,
-                          size_t Size) override {}
-    void deregisterEHFrames(uint8_t *Addr, uint64_t LoadAddr,
-                            size_t Size) override {}
-private:
-    std::map<std::string, uint64_t> DummyExterns;
 };
 
 uint8_t *TrivialMemoryManager::allocateCodeSection(uintptr_t Size,
@@ -172,6 +133,7 @@ void TrivialMemoryManager::invalidateInstructionCache() {
                                                 DataMemory[i].size());
 }
 
+
 int main(int argc, char** argv) {
 
     if(argc != 2) {
@@ -189,30 +151,27 @@ int main(int argc, char** argv) {
 
     LLVMContext Context;
     SMDiagnostic diag;
-    std::unique_ptr<MIRParser> MIR;
 
     std::string filePath = "data/" + matrixName + "/" + matrixName + "_CSRbyNZ/generated_merged.ll";
-    std::unique_ptr<Module> M = parseIRFile(filePath, diag, Context);
-    if (!M) {
+    std::unique_ptr<Module> M;
+    Module *mod = nullptr;
+    M.reset(ParseIRFile(filePath, diag, Context));
+    mod = M.get();
+    
+    if (mod == nullptr) {
         errs() << "No such matrix";
         return 1;
     }
-
-    // Begin LLC-like implementation
-    if (verifyModule(*M, &errs())) {
-        errs() << argv[0] << ": " << filePath
-        << ": error: input module is broken!\n";
-        return 1;
-    }
     
-    Triple TheTriple = Triple(M->getTargetTriple());
+    Triple TheTriple = Triple(mod->getTargetTriple());
 
     std::string Error;
     const Target *TheTarget = TargetRegistry::lookupTarget(MArch, TheTriple,
                                                            Error);
 
-    std::string CPUStr = getCPUStr(), FeaturesStr = getFeaturesStr();
-
+    std::string MCPU = "";
+    std::string FeaturesStr = "";
+    
     CodeGenOpt::Level OLvl = CodeGenOpt::Default;
     FileType = TargetMachine::CGFT_ObjectFile;
 
@@ -222,75 +181,59 @@ int main(int argc, char** argv) {
 //    Options.MCOptions.MCUseDwarfDirectory = EnableDwarfDirectory;
 //    Options.MCOptions.AsmVerbose = AsmVerbose;
 
-    std::unique_ptr<TargetMachine> Target(
-            TheTarget->createTargetMachine(TheTriple.getTriple(), CPUStr, FeaturesStr,
+    std::unique_ptr<TargetMachine> target(
+            TheTarget->createTargetMachine(TheTriple.getTriple(), MCPU, FeaturesStr,
                                            Options, RelocModel , CMModel, OLvl));
 
-    assert(Target && "Could not allocate target machine!");
+    TargetMachine &Target = *target.get();
+    
+    PassManager PM;
 
-    legacy::PassManager PM;
 
+    TargetLibraryInfo *TLI = new TargetLibraryInfo(TheTriple);
 
-    TargetLibraryInfoImpl TLII(Triple(M->getTargetTriple()));
+    PM.add(TLI);
 
-//    if (DisableSimplifyLibCalls)
-//        TLII.disableAllFunctions();
+    if (const DataLayout *DL = Target.getDataLayout())
+        mod->setDataLayout(DL);
 
-    PM.add(new TargetLibraryInfoWrapperPass(TLII));
-
-    if (const DataLayout *DL = Target->getDataLayout())
-        M->setDataLayout(*DL);
-
-    setFunctionAttributes(CPUStr, FeaturesStr, *M);
-  
     SmallVector<char, 1024*1024*4> *smallVector = new SmallVector<char, 1024*1024*4>();
     raw_svector_ostream svectorOS(*smallVector);
-    raw_pwrite_stream *OS = &svectorOS;
-  
-    // Ask the target to add backend passes as necessary.
-    /* Wrote "true" in place of "NoVerify" */
-    if (Target->addPassesToEmitFile(PM, *OS, FileType, true, nullptr,
-                                    nullptr, nullptr, MIR.get())) {
-        errs() << argv[0] << ": target does not support generation of this"
-        << " file type!\n";
-        return 1;
+    {
+        formatted_raw_ostream FOS(svectorOS);
+        
+        // Ask the target to add backend passes as necessary.
+        /* Wrote "true" in place of "NoVerify" */
+        if (Target.addPassesToEmitFile(PM, FOS, FileType, true, nullptr, nullptr)) {
+            errs() << argv[0] << ": target does not support generation of this"
+            << " file type!\n";
+            return 1;
+        }
+        
+        PM.run(*mod);
+        
+        svectorOS.flush();
     }
     
-    PM.run(*M);
     //outs() << svectorOS.str().str();
-    
-    svectorOS.flush();
-    
     outs() << "Generated the code.\n";
     
     ///////////////////////// Rtdyld Stuff //////////////////////////////
     
     // Instantiate a dynamic linker.
     TrivialMemoryManager MemMgr;
-    RuntimeDyld Dyld(MemMgr, MemMgr);
-    
-    // FIXME: Preserve buffers until resolveRelocations time to work around a bug
-    //        in RuntimeDyldELF.
-    // This fixme should be fixed ASAP. This is a very brittle workaround.
-    std::vector<std::unique_ptr<MemoryBuffer>> InputBuffers;
+    RuntimeDyld Dyld(&MemMgr);
     
     // Load the input memory buffer.
-    ErrorOr<std::unique_ptr<MemoryBuffer>> InputBuffer = MemoryBuffer::getMemBuffer(svectorOS.str());
-    ErrorOr<std::unique_ptr<ObjectFile>> MaybeObj(ObjectFile::createObjectFile((*InputBuffer)->getMemBufferRef()));
+    MemoryBuffer *memBuffer = MemoryBuffer::getMemBuffer(svectorOS.str(), "", false);
+    ObjectBuffer *Buffer = new ObjectBuffer(memBuffer);
+    
     outs() << "InputBuffer created.\n";
     
-    if (std::error_code EC = MaybeObj.getError()) {
-        errs() << "unable to create object file: '" + EC.message() + "'";
-        exit(1);
-    }
-    
-    ObjectFile &Obj = **MaybeObj;
-    InputBuffers.push_back(std::move(*InputBuffer));
-    
-    // Load the object file
-    Dyld.loadObject(Obj);
-    if (Dyld.hasError()) {
-        errs() << "Dyld error: " << Dyld.getErrorString();
+    // Load the input memory buffer.
+    ObjectImage *LoadedObject = Dyld.loadObject(Buffer);
+    if (!LoadedObject) {
+        errs() << "Dyld error:" << Dyld.getErrorString().str() << "\n";
         exit(1);
     }
     
@@ -304,9 +247,16 @@ int main(int argc, char** argv) {
     // FIXME: Error out if there are unresolved relocations.
     
     // Get the address of the entry point (_main by default).
-    void *MainAddress = Dyld.getSymbolLocalAddress("_multByM");
+    std::string multByMName;
+#ifdef __linux__
+    multByMName = "multByM";
+#else
+    multByMName = "_multByM";
+#endif
+    
+    void *MainAddress = Dyld.getSymbolAddress(multByMName);
     if (!MainAddress) {
-        errs() << "no definition for '_multByM'";
+        errs() << "no definition for '" << multByMName << "'";
         exit(1);
     }
     
@@ -346,7 +296,7 @@ int main(int argc, char** argv) {
     auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2).count();
     outs() << "Run function duration (microseconds): \n" << duration2 / 5000.0 << "\n";
     
-    
+    std::chrono::high_resolution_clock::time_point
     llvm_shutdown();
     return 0;
 }
